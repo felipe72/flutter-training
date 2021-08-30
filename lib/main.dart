500b13ec11b42d78816f635f0c5fcecb21721516
Felipe Osório <felipe.osorio72@outlook.com.br>
2021-08-30 16:44:34 -0300

    Add last invoice component

--- lib/main.dart
+++ lib/main.dart
@@ -32,8 +32,9 @@ class MyHomePage extends StatelessWidget {
         child: MyCardComponent(
           child: Column(
             children: <Widget>[
-              Row(
-                children: <Widget>[Text('Lorem Ipsum')],
+              LastInvoice(),
+              Divider(
+                color: Colors.black,
               )
             ],
           ),
@@ -54,10 +55,70 @@ class MyCardComponent extends StatelessWidget {
       child: Card(
         elevation: 2.0,
         child: Container(
-          padding: EdgeInsets.all(16.0),
+          padding: EdgeInsets.all(16),
           child: this.child,
         ),
       ),
+    );
+  }
+}
+
+class LastInvoice extends StatelessWidget {
+  LastInvoice({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Widget title = Text(
+      'Última fatura',
+      style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+    );
+
+    final Widget priceAndDate = AlignLeftColumn(
+      children: [
+        Text(
+          'R\$ 3.025,49',
+          style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          'Vencimento 08/07/2019',
+          style: TextStyle(fontSize: 20.0),
+        ),
+      ],
+    );
+
+    final Widget pastDue = Text(
+      'Vencida',
+      style: TextStyle(fontSize: 20.0, color: Colors.red),
+    );
+
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: AlignLeftColumn(
+        children: <Widget>[
+          title,
+          Container(
+            padding: EdgeInsets.only(top: 32.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[priceAndDate, pastDue],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AlignLeftColumn extends StatelessWidget {
+  AlignLeftColumn({Key key, this.children}) : super(key: key);
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: this.children,
     );
   }
 }
